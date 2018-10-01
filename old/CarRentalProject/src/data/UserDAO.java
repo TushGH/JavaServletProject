68f363fef3ac586060588ba27fc62e73a07e9ae2
Nalamalapuvarun <varun.nalamalapu@gmail.com>
2018-10-01 17:18:27 -0500

    Add files via upload
    Contains UpdateProfile, ViewMyReservation and CancelMyReservation functionalities

--- old/CarRentalProject/src/data/UserDAO.java
+++ old/CarRentalProject/src/data/UserDAO.java
@@ -4,90 +4,104 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import model.user;
+import java.util.ArrayList;
+import java.util.List;
 
+import model.ViewMyReservation;
 
 public class UserDAO {
-	
-	static Connection con;
-	static PreparedStatement ps;
-	public String role;
-	public int authentecateUser(String Username , String Password){
-		int status = 0 ;
-		System.out.println(Username);
-		String query = "select password,Role from user where username = ?";
+		static Connection con;
+		static PreparedStatement ps;
+		public void CancelReservationDAO(String confirmation) {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");  
-			   Connection con=DriverManager.getConnection(  
-			      "jdbc:mysql://localhost:3306/sakila","root","Prasi_1995");  
-	    	  ps=con.prepareStatement(query);
-	    	  ps.setString(1, Username);
-	    	  
-	      
-	        ResultSet rs = ps.executeQuery();
-	        
-	        rs.next();
-	        
-	        role=rs.getString("Role");
-	        //System.out.println(rs.getString(1));
-	            
-	      
-			   if(Password.equals(rs.getString("password"))){
-				   status = 1 ;
-				   
-			   }
-			   con.close();
-			
+			Connection con=DriverManager.getConnection(  
+				      "jdbc:mysql://localhost:3306/carrental","root","");  
+			String query="delete from reservedcars where Reservationid=?" ;
+			ps=con.prepareStatement(query);
+		    ps.setString(1,confirmation);
+		    boolean b=ps.execute();
+		    if(b) {
+		    	System.out.println("executed the query");
+		    }
+		    else {
+		    	System.out.println("Havenot executed the query");
+		    }
+		    con.close();
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-		
-	return status ;
 	}
-	public  int insertuser(user newuser) {
-		// TODO Auto-generated method stub
-		int status =0;
-		System.out.println("IN DAO");
 		
-		try
-		{   Class.forName("com.mysql.jdbc.Driver");  
-		   Connection con=DriverManager.getConnection(  
-		      "jdbc:mysql://localhost:3306/sakila","root","Prasi_1995");  
-		
+		public int UpdateDao(String name, String email, String phone, String addr, String newpassword, String username) {
+			// TODO Auto-generated constructor stub
+			int status = 0 ;
+			String role;
+			String query="UPDATE CARRENTAL.USER SET NAME=?,EMAIL=?,PHONE=?,ADDRESS=?,PASSWORD=? WHERE USERNAME=?";
+			try {
+				Class.forName("com.mysql.jdbc.Driver");  
+				java.sql.Connection con=DriverManager.getConnection(  
+				"jdbc:mysql://localhost:3306/carrental","root",""); 	      
+				          PreparedStatement preparedStmt = con.prepareStatement(query);
+					      preparedStmt.setString (1, name);
+					      preparedStmt.setString   (2, email); 
+					      preparedStmt.setString   (3, phone);
+					      preparedStmt.setString   (4, addr);
+					      preparedStmt.setString   (5, newpassword);
+					      preparedStmt.setString   (6, username);		      
+					      //if(newpassword==conpassword) {
+						      preparedStmt.executeUpdate();					        
+					      //}
+					      //else {
+					    	//  System.out.println("New password doesn't match");
+					      //}
+					      status=1;
+					      con.close();
+				 System.out.println("Database connection successful");		
+			}
 			
-			 ps=con.prepareStatement("insert into user values(?,?,?,?,?,?,?,?,?,?)");
-			 System.out.println(newuser.getUsername());
-			 System.out.println(newuser.getPassword());
-			 System.out.println(newuser.getFullName());
-			 System.out.println(newuser.getRole());
-			 System.out.println(newuser.getLicence());
-			 System.out.println(newuser.getAddress());
-			 System.out.println(newuser.getUTAID());
-			 System.out.println(newuser.getEmail());
-			 System.out.println(newuser.getPhoneNo());
-			 System.out.println(newuser.getAutoClub());
-		     ps.setString(1,newuser.getUsername());
-		     ps.setString(2, newuser.getPassword());
-		     ps.setString(3,newuser.getFullName() );
-		     ps.setString(4, newuser.getRole());
-		     ps.setString(5, newuser.getLicence());
-		     ps.setString(6, newuser.getAddress());
-		     ps.setString(7, newuser.getUTAID());
-		     ps.setString(8, newuser.getEmail());
-		     ps.setString(9,newuser.getPhoneNo() );
-		     ps.setString(10, newuser.getAutoClub());
-		     status=ps.executeUpdate();
-		     con.close();
-		}
-		catch(Exception e)
-		{
+			catch(Exception e)
+			{
+				System.out.println(e);
+				status=0;
+				System.out.println("Database connection unsuccessful");
+			}
+			return status;
+	}
+		
+		public List<ViewMyReservation> ViewReservationDAO(String startdate,String enddate){
+			ViewMyReservation viewMy=null;
+			List<ViewMyReservation> Result=new ArrayList<>();
+		String query="select * from reservedcars where startdate=? and enddate=?";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");  
+			Connection con=DriverManager.getConnection(  
+				      "jdbc:mysql://localhost:3306/carrental","root","");  
+			ps=con.prepareStatement(query);
+		    ps.setString(1, startdate);
+		    ps.setString(2, enddate);
+		    ResultSet rs1 = ps.executeQuery();
+		    while(rs1.next()) {
+		    	viewMy = new ViewMyReservation();
+		    	viewMy.setReservationid(rs1.getString("Reservationid"));
+		    	viewMy.setCarName(rs1.getString("CarName"));
+		    	viewMy.setStartTime(rs1.getString("StartTime"));
+		    	viewMy.setstartdate(rs1.getString("StartDate"));
+		    	viewMy.setEndTime(rs1.getString("EndTime"));
+		    	viewMy.setenddate(rs1.getString("EndDate"));
+		    	System.out.println(viewMy.getReservationid());
+		    	System.out.println(viewMy.getCarName());
+		    	System.out.println(viewMy.getStartTime());
+		    	System.out.println(viewMy.getstartdate());
+		    	System.out.println(viewMy.getEndTime());
+		    	System.out.println(viewMy.getenddate());
+		    	Result.add(viewMy);
+		    }
+		    con.close();
+//		    return rs;
+		} catch (Exception e) {
 			System.out.println(e);
 		}
-		
-		return status;
-	}
-	public static void main(String[] args) {
-		
-   }
-	
+		return Result;
+		}
 }
