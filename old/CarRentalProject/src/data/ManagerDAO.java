7610cf173e8b9314a1595a2f4790eb2d8e89dad4
Vamsi2396 <33335328+Vamsi2396@users.noreply.github.com>
2018-10-03 18:55:34 -0500

    Update ManagerDAO.java

--- old/CarRentalProject/src/data/ManagerDAO.java
+++ old/CarRentalProject/src/data/ManagerDAO.java
@@ -111,6 +111,62 @@ try {
 	System.out.println(e);
 }
 }
+public void delete(String reservationid) {
+	try {
+		Class.forName("com.mysql.jdbc.Driver");  
+		Connection con=DriverManager.getConnection(  
+			      "jdbc:mysql://localhost:3306/carrental","root","");  
+		String query="delete from reservedcars where Reservationid=?" ;
+		ps=con.prepareStatement(query);
+	    ps.setString(1,reservationid);
+	    boolean b=ps.execute();
+	    if(b) {
+	    	System.out.println("executed the query");
+	    }
+	    else {
+	    	System.out.println("Havenot executed the query");
+	    }
+	    con.close();
+	} catch (Exception e) {
+		System.out.println(e);
+	}
+}
+
+public int UpdateManager(String name, String email, String phone, String addr, String newpassword, String username) {
+	// TODO Auto-generated constructor stub
+	int status = 0 ;
+	String role;
+	String query="UPDATE CARRENTAL.USER SET NAME=?,EMAIL=?,PHONE=?,ADDRESS=?,PASSWORD=? WHERE USERNAME=?";
+	try {
+		Class.forName("com.mysql.jdbc.Driver");  
+		java.sql.Connection con=DriverManager.getConnection(  
+		"jdbc:mysql://localhost:3306/carrental","root",""); 	      
+		          PreparedStatement preparedStmt = con.prepareStatement(query);
+			      preparedStmt.setString (1, name);
+			      preparedStmt.setString   (2, email); 
+			      preparedStmt.setString   (3, phone);
+			      preparedStmt.setString   (4, addr);
+			      preparedStmt.setString   (5, newpassword);
+			      preparedStmt.setString   (6, username);		      
+			      //if(newpassword==conpassword) {
+				      preparedStmt.executeUpdate();					        
+			      //}
+			      //else {
+			    	//  System.out.println("New password doesn't match");
+			      //}
+			      status=1;
+			      con.close();
+		 System.out.println("Database connection successful");		
+	}
+	
+	catch(Exception e)
+	{
+		System.out.println(e);
+		status=0;
+		System.out.println("Database connection unsuccessful");
+	}
+	return status;
+}
 
 }
 
