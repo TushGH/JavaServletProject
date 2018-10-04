9500bfcd09c0f8012cc6312a2e3af217684b5326
Prasanna20times <36083814+Prasanna20times@users.noreply.github.com>
2018-10-04 00:40:28 -0500

    Add files via upload

---
+++ Searchcar.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Searchcar {
+private String StartDate;
+private String EndDate;
+private String StartTime;
+private String EndTime;
+private String Passengers;
+public String getStartDate() {
+	return StartDate;
+}
+public void setStartDate(String startDate) {
+	StartDate = startDate;
+}
+public String getEndDate() {
+	return EndDate;
+}
+public void setEndDate(String endDate) {
+	EndDate = endDate;
+}
+public String getStartTime() {
+	return StartTime;
+}
+public void setStartTime(String startTime) {
+	StartTime = startTime;
+}
+public String getEndTime() {
+	return EndTime;
+}
+public void setEndTime(String endTime) {
+	EndTime = endTime;
+}
+public String getPassengers() {
+	return Passengers;
+}
+public void setPassengers(String passengers) {
+	Passengers = passengers;
+}
+
+}
