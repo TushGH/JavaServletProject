2f77a2828538dc33dd1670418eb6821a575ae160
Vamsi2396 <33335328+Vamsi2396@users.noreply.github.com>
2018-10-03 22:47:16 -0500

    Update commands.sql

--- old/CarRentalProject/commands.sql
+++ old/CarRentalProject/commands.sql
@@ -17,9 +17,9 @@ CREATE TABLE cars (CarName varchar(100), capacity int, Weekday double, Weekend d
 CREATE TABLE reservedcars (Confirmationid int, CarName varchar(100), Username varchar(100), Starttime time, Startdate date, Endtime time, EndDate Date);
 
 
-INSERT INTO cars(CarName, capacity , Weekday, Weekend, Week)VALUES('Smart', '1','32.99','37.99','230.93'),
-('UltraSports', '2','199.99','204.99','1399.93','3','5','7'),
-('Economy', '3','39.99','44.99','279.93','5','7','9'),
+INSERT INTO cars(CarName, capacity , Weekday, Weekend, Week, GPS, OnStar, SiriusXm)VALUES('Smart', '1','32.99','37.99','230.93','3','5','7'),
+('UltraSports', '2','199.99','204.99','1399.93','5','7','9'),
+('Economy', '3','39.99','44.99','279.93','3','5','7'),
 ('Compact', '4','44.99','49.99','314.93','3','5','7'),
 ('Intermediate', '4','45.99','50.99','321.93','3','5','7'),
 ('Standard', '5','48.99','53.99','342.93','3','5','7'),
